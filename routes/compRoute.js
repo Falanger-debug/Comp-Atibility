b2dea36ba53141b6b90219893e8d990a9cc1e28f
Falanger-debug <falanger27@gmail.com>
2024-12-03 14:17:44 +0100

    working on comp.ejs

--- routes/compRoute.js
+++ routes/compRoute.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/comp', (req, res) => {
-    return res.status(200).json({dada: "dada"});
+    return res.render('comp.ejs');
 });
 
 export default router;
