d929593117a8c7a77ffd640f8b6a7c82da28d119
falan <falanger27@gmail.com>
2025-02-08 10:22:14 +0100

    Refactorized routes and controllers

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,9 +1,15 @@
 import express from 'express';
 
+import {
+    renderMainPage, renderComp, renderBuild, renderProducts
+} from '../controllers/mainController.js';
+
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    return res.render('build.ejs');
-});
+router.get('/', renderMainPage);
+router.get('/comp', renderComp);
+router.get('/build', renderBuild);
+router.get('/products', renderProducts);
 
 export default router;
