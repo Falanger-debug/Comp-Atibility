4f13868f856b49ca42911f42b0f8e86b01a5ecc3
falan <falanger27@gmail.com>
2025-02-13 12:24:45 +0100

    highlighting selected components

--- public/scripts/allowClickBasedOnUrl.js
+++ public/scripts/allowClickBasedOnUrl.js
@@ -1,6 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
     const path = window.location.pathname;
     const isBuildPage = path.startsWith("/build");
+    let build = JSON.parse(localStorage.getItem("pcBuild")) || {};
+
+    // Highlight selected components
+    document.querySelectorAll("tbody tr").forEach((row) => {
+        const button = row.querySelector(".choose-component");
+        if (!button) return;
+
+        const componentType = button.dataset.component;
+        const componentId = button.dataset.id;
+        const componentModel = button.dataset.model;
+
+        if (build[componentType] && build[componentType].id === componentId && build[componentType].model === componentModel) {
+            row.classList.add("table-warning");
+        }
+
+    });
+
 
     document.querySelectorAll(".choose-component").forEach((button) => {
         if (!isBuildPage) {
@@ -11,9 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
             button.addEventListener("click", function () {
                 const component = this.dataset.component;
                 const product = {
-                    id: this.dataset.id,
-                    brand: this.dataset.brand,
-                    model: this.dataset.model
+                    id: this.dataset.id, brand: this.dataset.brand, model: this.dataset.model
                 };
 
                 let build = JSON.parse(localStorage.getItem("pcBuild")) || {};
